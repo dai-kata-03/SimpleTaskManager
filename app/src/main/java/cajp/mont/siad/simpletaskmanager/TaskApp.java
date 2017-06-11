97334837519b5d62e163b13326fdde33e21b7192
Daisuke <siq009@yahoo.ca>
2017-06-11 11:17:13 -0400

    Initial commit

---
+++ app/src/main/java/cajp/mont/siad/simpletaskmanager/TaskApp.java
@@ -0,0 +1,13 @@
+package cajp.mont.siad.simpletaskmanager;
+
+import android.app.Application;
+
+import io.realm.Realm;
+
+public class TaskApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+    }
+}
